aacb0f9362d08c0953e2f47ed6b9788d18c5cf13
Johan Cortes <jako12287@gmail.com>
2023-02-02 18:02:43 -0500

    fix 012023 WAB - 0209 Route creation remove favorites

--- src/controllers/Favorites.controller.js
+++ src/controllers/Favorites.controller.js
@@ -2,7 +2,7 @@ import { User } from "../models/User.js";
 import { City } from "../models/City.js";
 
 export const addFavorites = async (req, res) => {
-  const {userId, cityId} = req.params;
+  const { userId, cityId } = req.params;
   try {
     const user = await User.findByPk(userId);
     const city = await City.findByPk(cityId);
@@ -17,3 +17,23 @@ export const addFavorites = async (req, res) => {
     res.status(500).send({ message: "Error adding city to favorites" });
   }
 };
+
+export const removeFavorites = async () => {
+  const { userId, cityId } = req.params;
+
+  try {
+    const user = await User.findByPk(userId);
+    const city = await City.findByPk(cityId);
+
+    if (!user || !city) {
+      return res.status(404).send({ message: "User or city not found" });
+    }
+
+    await user.removeCity(city);
+    res.send({ message: "City removed from favorites" });
+  } catch (error) {
+    res.status(500).send({ message: "Error removing city from favorites" });
+  }
+};
+
+
--- src/routes/Favorites.routes.js
+++ src/routes/Favorites.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addFavorites } from "../controllers/Favorites.controller.js";
+import { addFavorites, removeFavorites } from "../controllers/Favorites.controller.js";
 
 const router = Router();
 
 router.post("/favorites/:userId/:cityId", addFavorites);
+router.delete("/favorites/:userId/:cityId", removeFavorites);
 
 export default router;
